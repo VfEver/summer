df2fda788c92ecd4f962a2c34ce841988d6fc22a
VfEver <13146770925@126.com>
2018-01-10 23:45:21 +0800

    add summer context.  --zys  2018年1月10日23:45:11

--- summer-beans/src/main/java/com/summer/beans/factory/AbstractBeanFactory.java
+++ summer-beans/src/main/java/com/summer/beans/factory/AbstractBeanFactory.java
@@ -1,5 +1,6 @@
 package com.summer.beans.factory;
 
+import com.summer.beans.exception.BeanNotFindException;
 import com.summer.common.support.Assert;
 
 /**
@@ -10,5 +11,48 @@ import com.summer.common.support.Assert;
  */
 public abstract class AbstractBeanFactory extends SimpleAliasRegistry implements BeanFactory {
 
+    /**
+     * parent beanfactory, for bean inheritance support
+     */
+    private BeanFactory parentBeanFactory;
 
+    public AbstractBeanFactory () {
+
+        this(null);
+    }
+
+    public AbstractBeanFactory (BeanFactory parentBeanFactory) {
+
+        this.parentBeanFactory = parentBeanFactory;
+    }
+
+    @Override
+    public Object getBean(String name) throws BeanNotFindException {
+        return null;
+    }
+
+    @Override
+    public <T> T getBean(String id, Class<T> tClass) throws BeanNotFindException {
+        return null;
+    }
+
+    @Override
+    public Class<?> getType(String name) throws BeanNotFindException {
+        return null;
+    }
+
+    @Override
+    public boolean isSingleton(String name) throws BeanNotFindException {
+        return false;
+    }
+
+    @Override
+    public boolean isPrototype() throws BeanNotFindException {
+        return false;
+    }
+
+    @Override
+    public boolean containsBean(String name) {
+        return false;
+    }
 }
